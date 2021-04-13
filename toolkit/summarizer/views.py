630a6d941e20efdc8ed2ac2e36aa67ffbed7779d
wael <wael@texta.ee>
2021-04-13 18:04:42 +0300

    Summarizer views permissions and queryset

--- toolkit/summarizer/views.py
+++ toolkit/summarizer/views.py
@@ -1,16 +1,29 @@
+import rest_framework.filters as drf_filters
+from django_filters import rest_framework as filters
 from rest_framework import permissions, status, viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from .serializers import SummarizerSummerizeTextSerializer
+from .serializers import SummarizerSummarizeTextSerializer
+from toolkit.permissions.project_permissions import ProjectResourceAllowed
+from .models import Summarizer
+from toolkit.view_constants import BulkDelete
 
 
-class SummarizerViewSet(viewsets.ModelViewSet):
-    ordering_fields = ()
+class SummarizerViewSet(viewsets.ModelViewSet, BulkDelete):
+    serializer_class = SummarizerSummarizeTextSerializer
+    filter_backends = (drf_filters.OrderingFilter, filters.DjangoFilterBackend)
+    permission_classes = (
+        ProjectResourceAllowed,
+        permissions.IsAuthenticated,
+    )
 
-    @action(detail=True, methods=['post'], serializer_class=SummarizerSummerizeTextSerializer)
-    def summerize_text(self, request):
-        serializer = SummarizerSummerizeTextSerializer(data=request.data)
+    def get_queryset(self):
+        return Summarizer.objects.filter(project=self.kwargs['project_pk'])
+
+    @action(detail=True, methods=['post'], serializer_class=SummarizerSummarizeTextSerializer)
+    def summarize_text(self, request):
+        serializer = SummarizerSummarizeTextSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        # summerize text
+        # summarize text
         result = ""
         return Response(result, status=status.HTTP_200_OK)
