d69134cccfc2454e668dfcf5e0ca4daa78156ab6
wael <wael@texta.ee>
2021-04-15 21:54:44 +0300

    Summarizer Summarize Serializer

--- toolkit/summarizer/serializers.py
+++ toolkit/summarizer/serializers.py
@@ -11,3 +11,32 @@ class SummarizerSummarizeTextSerializer(serializers.ModelSerializer, ProjectReso
         fields = ('id',
                   'url',
                   'text')
+
+
+class SummarizerSummarizeSerializer(serializers.Serializer):
+    SUPPORTED_ALGORITHMS = (
+        "textrank",
+        "lexrank"
+    )
+    SUPPORTED_RATIOS = (
+        "0",
+        "0.1",
+        "0.2",
+        "0.3",
+        "0.4",
+        "0.5",
+        "0.6",
+        "0.7",
+        "0.8",
+        "0.9",
+        "1"
+    )
+    text = serializers.ListField(child=serializers.CharField(), required=True)
+    alogrithms = serializers.MultipleChoiceField(
+        choices=SUPPORTED_ALGORITHMS,
+        default=["lexrank"]
+    )
+    ratio = serializers.MultipleChoiceField(
+        choices=SUPPORTED_RATIOS,
+        default=["0.2"]
+    )
