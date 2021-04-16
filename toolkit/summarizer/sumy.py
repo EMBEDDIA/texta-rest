72c9e224d841e1eb472af8ed8953fe8836c1c9bb
wael <wael@texta.ee>
2021-04-16 09:33:49 +0300

    refactor if and elif no need for parentheses

--- toolkit/summarizer/sumy.py
+++ toolkit/summarizer/sumy.py
@@ -6,28 +6,28 @@ class Sumy:
     def get_summarizers(names):
         summarizers = {}
         for name in names:
-            if (name == "random"):
+            if name == "random":
                 from sumy.summarizers.random import RandomSummarizer
                 summarizers["random"] = RandomSummarizer(null_stemmer)
-            elif (name == "luhn"):
+            elif name == "luhn":
                 from sumy.summarizers.luhn import LuhnSummarizer
                 summarizers["luhn"] = LuhnSummarizer(stemmer=null_stemmer)
-            elif (name == "lsa"):
+            elif name == "lsa":
                 from sumy.summarizers.lsa import LsaSummarizer
                 summarizers["lsa"] = LsaSummarizer(stemmer=null_stemmer)
-            elif (name == "lexrank"):
+            elif name == "lexrank":
                 from sumy.summarizers.lex_rank import LexRankSummarizer
                 summarizers["lexrank"] = LexRankSummarizer(null_stemmer)
-            elif (name == "textrank"):
+            elif name == "textrank":
                 from sumy.summarizers.text_rank import TextRankSummarizer
                 summarizers["textrank"] = TextRankSummarizer(null_stemmer)
-            elif (name == "sumbasic"):
+            elif name == "sumbasic":
                 from sumy.summarizers.sum_basic import SumBasicSummarizer
                 summarizers["sumbasic"] = SumBasicSummarizer(null_stemmer)
-            elif (name == "kl-sum"):
+            elif name == "kl-sum":
                 from sumy.summarizers.kl import KLSummarizer
                 summarizers["kl-sum"] = KLSummarizer(null_stemmer)
-            elif (name == "reduction"):
+            elif name == "reduction":
                 from sumy.summarizers.reduction import ReductionSummarizer
                 summarizers["reduction"] = ReductionSummarizer(null_stemmer)
 
