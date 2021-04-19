2af0b0861244f15f937d30de9c3b0af62b581880
wael <wael@texta.ee>
2021-04-19 23:39:33 +0300

    update summarizer model

--- toolkit/summarizer/models.py
+++ toolkit/summarizer/models.py
@@ -1,13 +1,19 @@
+import json
 from django.db import models
 from django.contrib.auth.models import User
 from toolkit.core.project.models import Project
 from toolkit.constants import MAX_DESC_LEN
+from toolkit.elastic.index.models import Index
+from toolkit.elastic.tools.searcher import EMPTY_QUERY
 
 
 class Summarizer(models.Model):
-    text = models.CharField(max_length=MAX_DESC_LEN)
+    description = models.CharField(max_length=MAX_DESC_LEN)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    query = models.TextField(default=json.dumps(EMPTY_QUERY))
+    indices = models.ManyToManyField(Index)
+    fields = models.TextField(default=json.dumps([]))
 
     def __str__(self):
-        return self.text
+        return self.description
