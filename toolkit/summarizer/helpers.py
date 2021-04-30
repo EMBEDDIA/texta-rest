8430b055dbdbe0a35a725c881ac0b6cdae240713
wael <wael@texta.ee>
2021-04-30 09:44:28 +0300

    remove texta_facts from summarizer process_actions

--- toolkit/summarizer/helpers.py
+++ toolkit/summarizer/helpers.py
@@ -1,35 +1,27 @@
 import logging
 from typing import List
 from .sumy import Sumy
-from toolkit.elastic.tools.document import ElasticDocument
 from toolkit.elastic.tools.searcher import ElasticSearcher
 from toolkit.settings import INFO_LOGGER
 
 
-def process_actions(generator: ElasticSearcher, field_data: List[str], summarizer_class: Sumy, summarizer_id: int):
+def process_actions(generator: ElasticSearcher, field_data: List[str], ratio: float, algorithm: List[str], summarizer_class: Sumy, summarizer_id: int):
     counter = 0
     info_logger = logging.getLogger(INFO_LOGGER)
-
-    info_logger.info(f"Starting the processing of indices for MLP worker with ID of {summarizer_id}!")
+    info_logger.info(f"Starting the processing of indices for Summarizer worker with ID of {summarizer_id}!")
     for document_batch in generator:
         document_sources = [dict(hit["_source"]) for hit in document_batch]
-        summarizer_processed = summarizer_class.run_on_index(document_sources, doc_paths=field_data, ratio=0.2, algorithm=["lexrank"])
+        summarizer_processed = summarizer_class.run_on_index(document_sources, doc_paths=field_data, ratio=ratio, algorithm=algorithm)
 
         for index, summarizer_processed_document in enumerate(summarizer_processed):
             original_elastic_document = document_batch[index]
-
-            # Make sure that existing facts in the document and new ones don't overlap.
-            original_facts = original_elastic_document["_source"].get("texta_facts", [])
-            new_facts = summarizer_processed_document.get("texta_facts", [])
-            total_facts = [fact for fact in original_facts + new_facts if fact]
-            unique_facts = ElasticDocument.remove_duplicate_facts(total_facts)
 
             elastic_update_body = {
                 "_id": original_elastic_document["_id"],
                 "_index": original_elastic_document["_index"],
                 "_type": original_elastic_document.get("_type", "_doc"),
                 "_op_type": "update",
-                "doc": {**summarizer_processed_document, **{"texta_facts": unique_facts}}
+                "doc": {**summarizer_processed_document}
             }
 
             yield elastic_update_body
