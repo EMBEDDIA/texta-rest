65c76ffbe3dbe8b334c71860a5f52f581894a7f6
Wael <wael@texta.ee>
2021-05-03 14:13:51 +0300

    fix summarizer tests

--- toolkit/summarizer/tests/test_summarize_views.py
+++ toolkit/summarizer/tests/test_summarize_views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from rest_framework.test import APITestCase
 from toolkit.tools.utils_for_tests import create_test_user, print_output
 from toolkit.test_settings import TEST_VERSION_PREFIX
@@ -9,7 +10,7 @@ class SummarizerSummarizeViewTests(APITestCase):
     @classmethod
     def setUpTestData(cls):
         cls.user = create_test_user('user', 'my@email.com', 'pw')
-        cls.url = f'{TEST_VERSION_PREFIX}/summarizer_summarize/'
+        cls.url = reverse("v2:summarizer_summarize")
 
         cls.summarizer_id = None
 
@@ -21,13 +22,10 @@ class SummarizerSummarizeViewTests(APITestCase):
 
     def run_test_summarizer_summarize(self):
         payload = {
-            "text": "This is sentence 1. ###This is sentence 2. ###This is sentence 3. ###This is sentence 4.",
+            "text": "This is sentence 1. This is sentence 2. This is sentence 3. This is sentence 4.",
         }
 
         response = self.client.post(self.url, payload)
-        print_output('test_summarizer_summarize:response.data', response.data)
-        created_id = response.data['id']
+        print_output('test_summarizer_summarize:response', response)
 
-        self.summarizer_id = created_id
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
