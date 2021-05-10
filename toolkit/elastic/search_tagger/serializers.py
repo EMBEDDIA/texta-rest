f4ced3a8696bd4ccdd7200dcf0bf2d48368281dc
wael <wael@texta.ee>
2021-05-10 13:20:54 +0300

    serializers name change for query and fields search tagger

--- toolkit/elastic/search_tagger/serializers.py
+++ toolkit/elastic/search_tagger/serializers.py
@@ -1,11 +1,27 @@
-from .models import SearchTagger
+from .models import SearchQueryTagger, SearchFieldsTagger
 from rest_framework import serializers
 from toolkit.core.task.serializers import TaskSerializer
 from toolkit.elastic.index.serializers import IndexSerializer
 from toolkit.serializer_constants import FieldParseSerializer
 
 
-class SearchTaggerQuerySerializer(FieldParseSerializer, serializers.ModelSerializer):
+class SearchQueryTaggerSerializer(FieldParseSerializer, serializers.ModelSerializer):
+    indices = IndexSerializer(many=True, default=[])
+    author_username = serializers.CharField(source='author.username', read_only=True, required=False)
+    description = serializers.CharField()
+    task = TaskSerializer(read_only=True, required=False)
+    url = serializers.SerializerMethodField()
+    query = serializers.JSONField(help_text='Query in JSON format', required=False)
+    mapping_field = serializers.CharField(required=True)
+    fact_name = serializers.CharField()
+    fact_value = serializers.CharField()
+
+    class Meta:
+        model = SearchQueryTagger
+        fields = ("id", "url", "author_username", "indices", "description", "task", "query", "mapping_field", "fact_name", "fact_value")
+
+
+class SearchFieldsTaggerSerializer(FieldParseSerializer, serializers.ModelSerializer):
     indices = IndexSerializer(many=True, default=[])
     author_username = serializers.CharField(source='author.username', read_only=True, required=False)
     description = serializers.CharField()
@@ -13,23 +29,9 @@ class SearchTaggerQuerySerializer(FieldParseSerializer, serializers.ModelSeriali
     url = serializers.SerializerMethodField()
     query = serializers.JSONField(help_text='Query in JSON format', required=False)
     fields = serializers.ListField(child=serializers.CharField(), required=True)
+    fact_name = serializers.CharField()
 
     class Meta:
-        model = SearchTagger
-        fields = ("id", "url", "author_username", "indices", "description", "task", "query", "fields")
-        fields_to_parse = ['fields']
-
-
-class SearchTaggerFieldsSerializer(FieldParseSerializer, serializers.ModelSerializer):
-    indices = IndexSerializer(many=True, default=[])
-    author_username = serializers.CharField(source='author.username', read_only=True, required=False)
-    description = serializers.CharField()
-    task = TaskSerializer(read_only=True, required=False)
-    url = serializers.SerializerMethodField()
-    query = serializers.JSONField(help_text='Query in JSON format', required=False)
-    fields = serializers.ListField(child=serializers.CharField(), required=True)
-
-    class Meta:
-        model = SearchTagger
-        fields = ("id", "url", "author_username", "indices", "description", "task", "query", "fields")
+        model = SearchFieldsTagger
+        fields = ("id", "url", "author_username", "indices", "description", "task", "query", "fields", "fact_name")
         fields_to_parse = ['fields']
